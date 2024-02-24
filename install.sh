bde285d0668b5656aa7e6a79a34e4cdbb193e8bc
3xg3lin <krmutku76@gmail.com>
2024-02-24 15:53:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,6 +57,7 @@ then
     curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
     chmod u+x msfinstall
     ./msfinstall
+    rm msfinstall
 fi
 
 # Installation for Burp Suite
